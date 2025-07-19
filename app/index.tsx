58293fe2582ec5e01f99c68effbb4646e9d5394b
Ho Shing Lo <hlo015@users.noreply.github.com>
2025-07-19 23:05:47 +1200

    Added dark mode. Modified title.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { Stack } from 'expo-router';
+import { Text, View, useColorScheme } from "react-native";
 
 export default function Index() {
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === "dark";
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: isDark ? "#121212" : "#fff",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Stack.Screen
+        options={{
+          title: 'Score Counter',
+          headerStyle: { backgroundColor: isDark ? "#333333" : "#fff" },
+          headerTintColor: isDark ? "#fff" : "#000" ,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 30,
+          },
+          headerTitleAlign: 'center',
+        }}
+      />
+
+      <Text style={{ color: isDark ? "#fff" : "#000" }}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
